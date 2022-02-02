f5e2dc05d54363cd7f0664f8b29ed55aafd7f87f
hurricane2029 <hurricane2029@gmail.com>
2022-02-02 16:59:16 -0500

    added test listener code

--- subscriber.py
+++ subscriber.py
@@ -1,4 +1,5 @@
 import rospy
+import tf
 from std_msgs.msg import String
 
 def callback(data):
@@ -8,3 +9,15 @@ def listener():
     rospy.init_node('tf_listener')  # TODO: write node
     rospy.Subscriber("chatter", String, callback)
     rospy.spin()
+
+if __name__ == '__main__'
+    rate = rospy.Rate(10.0)
+    listener = tf.TransformListener()
+    while not rospy.is_shutdown():
+        try:
+            (trans, rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+
+    print(trans)
+    print(rot)
