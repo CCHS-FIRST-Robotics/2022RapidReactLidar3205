cd7ef563fd7d36787946e8e712eb3dc014a4e33c
hurricane2029 <hurricane2029@gmail.com>
2022-02-02 16:52:51 -0500

    test

--- subscriber.py
+++ subscriber.py
@@ -1,18 +1,10 @@
 import rospy
-import rospkg
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('/tf', String, queue_size = 10)
-    rospy.init_node('talker', anonymous = True)
-    rate = rospy.Rate(10)  # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+def callback(data):
+    rospy.loginfo("Heard: %s", data.data)
+
+def listener():
+    rospy.init_node('tf_listener')  # TODO: write node
+    rospy.Subscriber("chatter", String, callback)
+    rospy.spin()
