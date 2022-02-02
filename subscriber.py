a0c926123f5f0395b820e47568854ee4df27391a
hurricane2029 <hurricane2029@gmail.com>
2022-02-02 17:16:48 -0500

    added test code

--- subscriber.py
+++ subscriber.py
@@ -2,22 +2,15 @@ import rospy
 import tf
 from std_msgs.msg import String
 
-def callback(data):
+def callback(data): # logs info heard from listener
+    print(data.data)
     rospy.loginfo("Heard: %s", data.data)
 
 def listener():
-    rospy.init_node('tf_listener')  # TODO: write node
-    rospy.Subscriber("chatter", String, callback)
+    rospy.init_node('tf_listener')
+    rospy.Subscriber("chatter", String, callback) # when data is received, parses data into callback classes
     rospy.spin()
 
-if __name__ == '__main__'
-    rate = rospy.Rate(10.0)
-    listener = tf.TransformListener()
-    while not rospy.is_shutdown():
-        try:
-            (trans, rot) = listener.lookupTransform('/turtle2', '/turtle1', rospy.Time(0))
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
-
-    print(trans)
-    print(rot)
+if __name__ == '__main__':
+    # listener()
+    rospy.loginfo("Daniel is cute")
