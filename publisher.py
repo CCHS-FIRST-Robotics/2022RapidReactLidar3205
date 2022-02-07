44ee350c2dfcbfb8eea941c2ea9fa94d52f2e981
hurricane2029 <hurricane2029@gmail.com>
2022-02-07 16:18:57 -0500

    new tf debug branch

--- publisher.py
+++ publisher.py
@@ -3,7 +3,7 @@ from std_msgs.msg import String
 
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
+    rospy.init_node('talker', anonymous=True) # problem: init_node cannot be called twice
     rate = rospy.Rate(10) # 10hz
     #while not rospy.is_shutdown():
     hello_str = "hello world %s" % rospy.get_time() # gets current time as float seconds
--- subscriber.py
+++ subscriber.py
@@ -1,15 +1,17 @@
 import rospy
 import publisher as pb
 from std_msgs.msg import String
+import tf2_ros
+import tf2_msgs.msg
 
 def callback(data): # logs info heard from listener
     # print(data.data)
     rospy.loginfo("Heard: %s", data.data)
 
 def listener():
-    pb.talker()
+    #pb.talker()
     rospy.init_node('tf_listener')
-    rospy.Subscriber("chatter", String, callback) # when data is received, parses data into callback classes
+    rospy.Subscriber("tf", tf2_msgs.msg.TFMessage, callback) # when data is received, parses data into callback classes
     rospy.spin()
 
 if __name__ == '__main__':
