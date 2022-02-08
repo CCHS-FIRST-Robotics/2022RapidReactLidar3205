66451066cdb3864c8514dcf15e01cef7be05aa43
pereos <danieljiang2358@gmail.com>
2022-02-08 16:49:36 -0500

    Bug fix in subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -2,24 +2,21 @@ import rospy
 import tf2_msgs.msg
 import network as nt
 
-index = 0
 time_last_sent = 0
+
 def callback(data):  # logs info heard from listener
-    # print(data.data)
+    import subscriber as sb # callback function is isolated so this is necessary to access some variables
+
     trans = data.transforms[1].transform.translation  # X and Y are relative pos coords, z = 0
     rot = data.transforms[1].transform.rotation  # z = rotation in radians/pi ----- w = constant
     sex = data.transforms[1].header.stamp.secs
 
-    if index == 0:
-        time_last_sent = sex
-        index = 1
-    dt = sex - time_last_sent
+    if sb.time_last_sent == 0:
+        sb.time_last_sent = sex
+    dt = sex - sb.time_last_sent
 
     nt.send_data_to_table(trans, rot, dt)
-
-
-
-    time_last_sent = sex
+    sb.time_last_sent = sex
 
 def listen():
     rospy.init_node('tf_listener')
