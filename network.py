7c284f2889e53e0d02cb4f3f2c5fd9c7c7eae128
hurricane2029 <hurricane2029@gmail.com>
2022-02-09 16:06:12 -0500

    removed /w

--- network.py
+++ network.py
@@ -4,9 +4,9 @@ NetworkTables.initialize()
 table = NetworkTables.getTable("lidar")
 
 def send_data_to_table(trans, rot, dt):
-    table.putNumber("p_x_pos", trans.x / rot.w)
-    table.putNumber("p_y_pos", trans.y / rot.w)
-    table.putNumber("p_heading", rot.x / rot.w)
+    table.putNumber("p_x_pos", trans.x)
+    table.putNumber("p_y_pos", trans.y)
+    table.putNumber("p_heading", rot.x)
     table.putNumber("dt", dt)
     
 
