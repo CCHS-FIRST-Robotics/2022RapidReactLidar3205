63443349ae1604079286c57a098c9b5e2e3ea8b0
hurricane2029 <hurricane2029@gmail.com>
2022-02-09 15:34:56 -0500

    fixed bugs

--- subscriber.py
+++ subscriber.py
@@ -7,15 +7,15 @@ def callback(data):  # logs info heard from listener
     # print(data.data)
     trans = data.transforms[1].transform.translation  # X and Y are relative pos coords, z = 0
     rot = data.transforms[1].transform.rotation  # z = rotation in radians/pi ----- w = constant
-    sex = data.transforms[1].header.stamp.secs
+    nsex = data.transforms[1].header.stamp.nsecs
 
     if vars.time_last_sent == 0:
-        vars.time_last_sent = sex
+        vars.time_last_sent = nsex
 
-    dt = sex - vars.time_last_sent
+    dt = nsex - vars.time_last_sent
 
-    nt.sendDataToTable(trans, rot, dt, sex)
-    vars.time_last_sent = sex
+    nt.sendDataToTable(trans, rot, dt, nsex)
+    vars.time_last_sent = nsex
 
 def listen():
     rospy.init_node('tf_listener')
