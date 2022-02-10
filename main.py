c85ded8be73f49c2e4d18ecfc1881091c6633a7c
pereos <danieljiang2358@gmail.com>
2022-02-10 11:49:49 -0500

    topic name fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from multiprocessing import Process
 import tf_handler as tfh  # translation data processing
-import sub_map_handler as smp  # obstacle mapping data processing
+import submap_handler as smp  # obstacle mapping data processing
 
 
 if __name__ == '__main__':
