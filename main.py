3611ca4351cca27e735adecdfd2b5e1abf7be1d5
pereos <danieljiang2358@gmail.com>
2022-02-11 12:43:50 -0500

    testing

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import os
-import subprocess
+import multiprocessing as mp
 import network as nt
 import tf_handler as tf
 import submap_handler as sm
 
 path = "~/catkin_ws"
-os.system("." + path + "/devel/setup.sh")
+os.system(". " + path + "/devel/setup.sh")
+
+def reset_ros():
+    os.system("roslaunch gbot_core gbot.launch")
 
 reset = False # Resets ROS if it receives True value over network tables
-ros = subprocess.Popen(["roslaunch gbot_core gbot.launch"], shell=True)
+proc = mp.Process(target=reset_ros)
+proc.start()
 
 while True:
     if not reset:
         if nt.get_reset():
-            ros.terminate()
-            ros = subprocess.Popen(["roslaunch gbot_core gbot.launch"], shell=True)
-            
+            proc.kill()
+            proc.close()
+            proc.start()
             reset = True
     else:
         if not nt.get_reset():
