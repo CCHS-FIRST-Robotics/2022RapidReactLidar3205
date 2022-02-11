3c6a6feeba50b35a5bc6972bcf5225b72cba12f3
pereos <danieljiang2358@gmail.com>
2022-02-11 15:13:52 -0500

    fix rospy.init_node issue

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
+import rospy
 import multiprocessing as mp
 import network as nt
 import tf_handler as tf
 import submap_handler as sm
 
-path = "~/catkin_ws"
+path = "~/Desktop/Lidar" # TEMP PATH
 
 def reset_ros():
     os.system(". " + path + "/devel/setup.sh && roslaunch gbot_core gbot.launch")
+
+rospy.init_node('', anonymous=True)
 
 reset = False # Resets ROS if it receives True value over network tables
 proc = mp.Process(target=reset_ros)
