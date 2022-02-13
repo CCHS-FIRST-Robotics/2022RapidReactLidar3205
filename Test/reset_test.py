c9e5c67d1381e09a43c9bf1fc685ec9545947e37
pereos <danieljiang2358@gmail.com>
2022-02-13 00:16:22 -0500

    python2 is simply the best

--- Test/reset_test.py
+++ Test/reset_test.py
@@ -5,7 +5,7 @@ NetworkTables.initialize()
 table = NetworkTables.getTable("lidar")
 
 while True:
-    input("PRESS ENTER")
+    raw_input("PRESS ENTER...")
     table.putBoolean("reset", True)
     time.sleep(1)
     table.putBoolean("reset", False)
