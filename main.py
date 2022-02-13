2066a5bf8e9e3e9a2824e47a3d4c4aa8efdb76ed
pereos <danieljiang2358@gmail.com>
2022-02-12 23:22:24 -0500

    waits for ros to start before reading from topic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import time
 import rospy
+import socket
 import subprocess
 from threading import Thread
 
@@ -9,15 +11,28 @@ import submap_handler as sm
 
 path = "~/catkin_ws" # TEMP PATH
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def wait_for_ros(): # Waits for ROS port to open before reading from topics
+    online = False
+    
+    while not online:
+        if sock.connect_ex(('127.0.0.1', 11311)) == 0:
+            online = True
+        else:
+            time.sleep(1)
+            
 reset = False # Resets ROS if it receives True value over network tables
-ros = subprocess.Popen([". " + path + "/devel/setup.sh && roslaunch gbot_core gbot.launch"], shell=True)
+
 rospy.init_node('node')
+ros = subprocess.Popen([". " + path + "/devel/setup.sh && roslaunch gbot_core gbot.launch"], shell=True)
+wait_for_ros()
 
 while True:
     if not reset:
         if nt.get_reset():
             ros.kill()
             ros = subprocess.Popen([". " + path + "/devel/setup.sh && roslaunch gbot_core gbot.launch"], shell=True)
+            wait_for_ros()
             reset = True
     else:
         if not nt.get_reset():
