8720cc25c4690654798d75e65c29fd00158612b4
pereos <danieljiang2358@gmail.com>
2022-02-13 19:28:06 -0500

    made combo test not crappy

--- Test/combo_test.py
+++ Test/combo_test.py
@@ -4,20 +4,16 @@ import time
 NetworkTables.initialize()
 table = NetworkTables.getTable("lidar")
 
-print(table.getNumber("p_x_pos", -1))
-print(table.getNumber("p_y_pos", -1))
-print(table.getNumber("p_heading", -1))
-print(table.getNumber("dt", -1))
-time.sleep(1)
-print(table.getNumber("p_x_pos", -1))
-print(table.getNumber("p_y_pos", -1))
-print(table.getNumber("p_heading", -1))
-print(table.getNumber("dt", -1))
-table.putBoolean("reset", True)
-time.sleep(1)
-table.putBoolean("reset", False)
-time.sleep(5)
-print(table.getNumber("p_x_pos", -1))
-print(table.getNumber("p_y_pos", -1))
-print(table.getNumber("p_heading", -1))
-print(table.getNumber("dt", -1))
+while True:
+    option = raw_input("1. DATA | 2. RESET...")
+    if option == 1:
+        print(table.getNumber("p_x_pos", -1))
+        print(table.getNumber("p_y_pos", -1))
+        print(table.getNumber("p_heading", -1))
+        print(table.getNumber("dt", -1))
+    elif option == 2:
+        table.putBoolean("reset", True)
+        time.sleep(1)
+        table.putBoolean("reset", False)
+    else:
+        print("INVALID")
