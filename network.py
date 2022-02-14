38c040e92399208e8cf7eae74e2429fa89f830ed
pereos <danieljiang2358@gmail.com>
2022-02-14 17:03:06 -0500

    added function for getting robot state over network tables

--- network.py
+++ network.py
@@ -2,14 +2,28 @@ import var
 from networktables import NetworkTables
 
 NetworkTables.initialize(server=var.ip)
-table = NetworkTables.getTable("lidar")
+
+lidar = NetworkTables.getTable("lidar")
+state = NetworkTables.getTable("State")
 
 def tf_data(trans, rot, dt):
-    table.putNumber("p_x_pos", trans.x)
-    table.putNumber("p_y_pos", trans.y)
-    table.putNumber("p_heading", rot.z)
-    table.putNumber("dt", dt)
+    lidar.putNumber("p_x_pos", trans.x)
+    lidar.putNumber("p_y_pos", trans.y)
+    lidar.putNumber("p_heading", rot.z)
+    lidar.putNumber("dt", dt)
     
 def get_reset():
-    reset = table.getBoolean("reset", False)
+    reset = lidar.getBoolean("reset", False)
     return reset
+
+def get_state():
+    vals = []
+
+    vals.append(state.getNumber("x_pos", 0.0))
+    vals.apeend(state.getNumber("y_pos", 0.0))
+    vals.append(state.getNumber("heading", 0.0))
+    vals.append(state.getNumber("x_vel", 0.0))
+    vals.append(state.getNumber("y_vel", 0.0))
+    vals.append(state.getNumber("a_vel", 0.0))
+
+    return vals
