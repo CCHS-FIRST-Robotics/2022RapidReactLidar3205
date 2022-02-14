85e2b685838643f434f49149988f44576c7cf11f
pereos <danieljiang2358@gmail.com>
2022-02-13 19:29:02 -0500

    fix

--- Test/combo_test.py
+++ Test/combo_test.py
@@ -6,12 +6,12 @@ table = NetworkTables.getTable("lidar")
 
 while True:
     option = raw_input("1. DATA | 2. RESET...")
-    if option == 1:
+    if option == "1":
         print(table.getNumber("p_x_pos", -1))
         print(table.getNumber("p_y_pos", -1))
         print(table.getNumber("p_heading", -1))
         print(table.getNumber("dt", -1))
-    elif option == 2:
+    elif option == "2":
         table.putBoolean("reset", True)
         time.sleep(1)
         table.putBoolean("reset", False)
