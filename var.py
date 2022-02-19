bd576f0442ef60ae488c799cd6d41f67613377f9
pereos <danieljiang2358@gmail.com>
2022-02-19 14:48:12 -0500

    imma dumb dumb

--- var.py
+++ var.py
@@ -1,4 +1,4 @@
 import os
 
-path = os.path.expanduser("~") + "/2022RapidReactLidar3205/Cartographer"
+path = "/home/ubuntu/2022RapidReactLidar3205/Cartographer" # Home directory name MUST be "ubuntu"
 ip = "127.0.0.1"
