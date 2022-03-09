738d95fd553b27883ece2c31c9bea6d57d63e5c8
pereos <danieljiang2358@gmail.com>
2022-03-09 16:00:00 -0500

    big ol odom refactor

--- odom_handler.py
+++ odom_handler.py
@@ -1,6 +1,7 @@
 import tf
 import rospy
 import network as nw
+from math import sin, cos, pi
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
@@ -9,16 +10,40 @@ def talk():
     odom_tf = tf.TransformBroadcaster()
     rospy.init_node('odom_talker', anonymous=True)
 
-    old_vars = []
-    while True:
-        new_vars = nw.get_state() # x_pos, y_pos, heading, x_vel, y_vel, a_vel
+    x = 0.0
+    y = 0.0
+    th = 0.0
+    
+    last_vars = []
+    current_time = rospy.Time.now()
+    last_time = rospy.Time.now()
 
-        if new_vars != old_vars:
+    while True:
+        current_vars = nw.get_state() # x_pos (0), y_pos (1), heading (2), x_vel (3), y_vel (4), a_vel (5)
+
+        if current_vars != last_vars:
+            x_pos  = current_vars[0]
+            y_pos  = current_vars[1]
+            th_pos = current_vars[2]
+            x_vel  = current_vars[3]
+            y_vel  = current_vars[4]
+            a_vel  = current_vars[5]
+
             current_time = rospy.Time.now()
-            odom_quat = tf.transformations.quaternion_from_euler(0, 0, new_vars[2])
+            dt = (current_time - last_time).to_sec()
+
+            delta_x = (x_vel * cos(th_pos) - y_vel * sin(th_pos)) * dt
+            delta_y = (x_vel * sin(th_pos) + y_vel * cos(th_pos)) * dt
+            delta_th = a_vel * dt
+
+            x += delta_x
+            y += delta_y
+            th += delta_th
+
+            odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
 
             odom_tf.sendTransform(
-                (new_vars[0], new_vars[1], 0.),
+                (x, y, 0.),
                 odom_quat,
                 current_time,
                 "base_link",
@@ -31,11 +56,11 @@ def talk():
             odom.header.stamp = current_time
             odom.header.frame_id = "odom"
 
-            odom.pose.pose = Pose(Point(new_vars[0], new_vars[1], 0.), Quaternion(*odom_quat))
-            odom.twist.twist = Twist(Vector3(new_vars[3], new_vars[4], 0), Vector3(0, 0, new_vars[5]))
+            odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+            odom.twist.twist = Twist(Vector3(x_vel, y_vel, 0), Vector3(0, 0, a_vel))
 
             odom_pub.publish(odom)
-            new_vars = old_vars
+            current_vars = last_vars
 
 if __name__ == "__main__":
     talk()
